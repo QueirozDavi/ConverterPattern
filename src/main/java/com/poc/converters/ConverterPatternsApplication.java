b9534a6ff2b85a56c8edb91ae2f6d99bbc0280f1
Davi Queiroz <davi.demeloqueiroz-ext@ab-inbev.com>
2021-12-14 11:12:01 -0300

    Initial Commit

---
+++ src/main/java/com/poc/converters/ConverterPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.poc.converters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterPatternsApplication.class, args);
+	}
+
+}
